100e8ebdd9c520c17e379d7a72d5fb14219f692a
Dar Hershkovits <darhers89@gmail.com>
2021-05-09 19:18:04 -0400

    final v2.0

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -34,26 +34,27 @@ buttons[0].addEventListener("click", () => {
     },500);
 });
 
-const refresh = setInterval(() => {
-    const dateNow = new Date().getTime();
-    const difference = launchDate - dateNow;
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+// const refresh = setInterval(() => {
+//     const dateNow = new Date().getTime();
+//     const difference = launchDate - dateNow;
+//     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     const mins = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+//     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    countdown.innerHTML = `
-        <div>${seconds}<span>שניות</span></div>
-        <div>${mins}<span>דקות</span></div>
-        <div>${hours}<span>שעות</span></div>        
-        <div>${days}<span>ימים</span></div>
-    `
+//     countdown.innerHTML = `
+//         <div>${seconds}<span>שניות</span></div>
+//         <div>${mins}<span>דקות</span></div>
+//         <div>${hours}<span>שעות</span></div>        
+//         <div>${days}<span>ימים</span></div>
+//     `
 
-    if (difference < 0) {
-        clearInterval(refresh);
-        countdown.innerHTML = "success!";
-    }
-}, 1000);
+//     if (difference < 0) {
+//         clearInterval(refresh);
+//         countdown.innerHTML = "success!";
+//     }
+// }, 1000);
+
 const images = [];
 for (let i=1; i<=15; i++) {
     images.push('url("/images/backgrounds/bg' + i +'.jpg")');
