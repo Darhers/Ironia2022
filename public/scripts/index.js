9123c914163138eb150f1a9d2ce9d70fabd0f891
Dar Hershkovits <darhers89@gmail.com>
2021-05-23 19:46:06 -0400

    Bootstrap revision - final v3.0

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,9 @@
 const countdown = document.getElementsByClassName("countdown")[0];
 const landingDiv = document.getElementsByClassName("landing")[0];
 const landingInnerDiv = document.getElementsByClassName("landing-inner")[0];
-const buttons = document.getElementsByClassName("btn-animated");
+const buttons = document.getElementsByClassName("btn");
 const headersDiv = document.getElementById("headers");
-const sponsorshipsDiv = document.getElementsByClassName("sponsorships")[0];
+const sponsorshipsDiv = document.getElementById("sponsorships");
 const launchDate = new Date("May 11, 2021 13:00:00").getTime();
 const imagesDiv = document.getElementById("images");
 const joinBtns = document.getElementsByClassName("join-link");
@@ -21,44 +21,8 @@ buttons[0].addEventListener("click", () => {
     },500);
     setTimeout(() => {
         sponsorshipsDiv.classList.toggle("hidden");
-        // if (landingDiv.style.height === "100vh") {
-        //     landingDiv.style.height = "50vh";
-        //     landingInnerDiv.style.height = "50vh";
-        // } else {
-        //     landingDiv.style.height = "100vh";
-        //     landingInnerDiv.style.height = "100vh";
-        // }
-        // let overflowSet = document.getElementsByTagName("body")[0].style.overflowY;
-        // console.log(overflowSet);
-        // // if (overflowSet === "auto") {
-        // //     overflowSet = "hidden";
-        // // } else {
-        // //     overflowSet = "auto";
-        // // }
-        // // document.getElementsByTagName("body")[0].style.overflowY = overflowSet;
     },500);
 });
-
-// const refresh = setInterval(() => {
-//     const dateNow = new Date().getTime();
-//     const difference = launchDate - dateNow;
-//     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-//     const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     const mins = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-//     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-//     countdown.innerHTML = `
-//         <div>${seconds}<span>שניות</span></div>
-//         <div>${mins}<span>דקות</span></div>
-//         <div>${hours}<span>שעות</span></div>        
-//         <div>${days}<span>ימים</span></div>
-//     `
-
-//     if (difference < 0) {
-//         clearInterval(refresh);
-//         countdown.innerHTML = "success!";
-//     }
-// }, 1000);
 
 const images = [];
 for (let i=1; i<=15; i++) {
