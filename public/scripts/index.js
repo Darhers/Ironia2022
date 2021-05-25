93af6d88f8227b51fb449bd6f5a3d8e2780f7425
Dar Hershkovits <darhers89@gmail.com>
2021-05-25 15:33:56 -0400

    add countdown

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,12 +4,14 @@ const landingInnerDiv = document.getElementsByClassName("landing-inner")[0];
 const buttons = document.getElementsByClassName("btn");
 const headersDiv = document.getElementById("headers");
 const sponsorshipsDiv = document.getElementById("sponsorships");
-const launchDate = new Date("May 11, 2021 13:00:00").getTime();
+const launchDate = new Date("May 28, 2021 8:00:00").getTime();
 const imagesDiv = document.getElementById("images");
 const joinBtns = document.getElementsByClassName("join-link");
 
+
 for (let i=0; i<joinBtns.length; i++) {
     joinBtns[i].href= "https://cntr.click/7YRa3Wt";
+    joinBtns[i].classList.add("hidden");
 }
 
 buttons[0].addEventListener("click", () => {
@@ -23,3 +25,25 @@ buttons[0].addEventListener("click", () => {
         sponsorshipsDiv.classList.toggle("hidden");
     },500);
 });
+
+const refresh = setInterval(() => {
+    const dateNow = new Date().getTime();
+    const difference = launchDate - dateNow;
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    countdown.innerHTML = `
+        <div class="col-1 ms-1 me-1 p-1 border border-dark rounded bg-primary bg-gradient text-dark fw-bold display-3">${seconds}<span>שניות</span></div>
+        <div class="col-1 ms-1 me-1 p-1 border border-dark rounded bg-primary bg-gradient text-dark fw-bold display-3">${mins}<span>דקות</span></div>
+        <div class="col-1 ms-1 me-1 p-1 border border-dark rounded bg-primary bg-gradient text-dark fw-bold display-3">${hours}<span>שעות</span></div>        
+        <div class="col-1 ms-1 me-1 p-1 border border-dark rounded bg-warning bg-gradient text-dark fw-bold display-3">${days}<span>ימים</span></div>
+    `
+
+    if (difference < 0) {
+        clearInterval(refresh);
+        countdown.innerHTML = "success!";
+    }
+}, 1000);
+
